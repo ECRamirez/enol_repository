0660720b1989cff17393b554e47eee94aa6f6448
enol <enol@debian-enol.debian-enol.com>
2018-12-06 23:25:48 +0100

    Commit inicial web spring sin web.xml ni dispatcher.servlet

---
+++ DebianP4WEb/src/main/java/com/enol/config/AppConfig.java
@@ -0,0 +1,10 @@
+package com.enol.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.enol")
+public class AppConfig {
+
+}
